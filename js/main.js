30265216473b7059de19276fd0ae2108ad214905
khaledahmedali <122289488+khaledahmedali@users.noreply.github.com>
2024-12-20 23:38:07 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let header = document.querySelector("header");
+
+window.onscroll = function(){
+    if(this.scrollY >= 50){
+        header.classList.add("active")
+    }
+    else{
+        header.classList.remove("active")
+    }
+}
+let nav_links =document.getElementById("links");
+function open_colose_menu(){
+    nav_links.classList.toggle("active")
+}
